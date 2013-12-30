22d889f0c78139e18115504f272f3551ebaf87a4
bartes <bartesrlz@gmail.com>
2013-12-30 13:03:04 +0100

    new plugins

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -6,13 +6,4 @@
 
 #cd `dirname $0`/vim/bundle/Command-T/ruby/command-t/; ruby extconf.rb; make
 
-brew install cmake
-brew install macvim
-brew install ack
-brew install ctags-exuberant
-brew install yajl
-brew install zsh
-brew install node
-brew install zsh
-brew install tmux
-brew install hub
+brew install postgresql mysql sqlite imagemagick nvm ctags zsh tmux hub ack cmake macvim ack yajl
