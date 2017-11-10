9c89e6844a85dc65747f8f794063fefc0f1d3530
bartes <bartesrlz@gmail.com>
2017-11-10 18:50:39 +0100

    update readme and dependencies

--- install_command_t.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-##################################
-# Various Dependencies
-##################################
-
-cd `dirname $0`/vim/bundle/Command-T/ruby/command-t/; ruby extconf.rb; make
-
--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,6 +4,4 @@
 # Various Dependencies
 ##################################
 
-#cd `dirname $0`/vim/bundle/Command-T/ruby/command-t/; ruby extconf.rb; make
-
-brew install postgresql mysql sqlite imagemagick nvm ctags zsh tmux hub ack cmake macvim ack yajl
+brew install imagemagick nvm ctags zsh tmux hub ack cmake macvim ack yajl chromedriver ctags curl the_silver_searcher
